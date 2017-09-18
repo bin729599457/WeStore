8717284a30c695655f64b3d9bb895fff5ec1913b
chenyi <717572168@qq.com>
2017-09-18 10:02:22 +0800

    Building web development environment(SpringMVC,MyBatis,Spring)

---
+++ src/main/java/com/westore/utils/RequestUtils.java
@@ -0,0 +1,52 @@
+package com.westore.utils;
+
+import org.apache.http.HttpEntity;
+import org.apache.http.ParseException;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
+import org.apache.http.util.EntityUtils;
+
+
+import java.io.IOException;
+
+public class RequestUtils {
+    public static String httpRequest(String url) {
+        String result = null;
+        CloseableHttpClient httpclient = HttpClients.createDefault();
+        try {
+            // 创建httpget.
+            HttpGet httpget = new HttpGet(url);
+            System.out.println("executing request " + httpget.getURI());
+            // 执行get请求.
+            CloseableHttpResponse response = httpclient.execute(httpget);
+            try {
+                // 获取响应实体
+                HttpEntity entity = response.getEntity();
+                // 打印响应状态
+                System.out.println(response.getStatusLine());
+                if (entity != null) {
+                    result = EntityUtils.toString(entity);
+                }
+            } finally {
+                response.close();
+            }
+        } catch (ClientProtocolException e) {
+            e.printStackTrace();
+        } catch (ParseException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            // 关闭连接,释放资源
+            try {
+                httpclient.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        return result;
+    }
+}
