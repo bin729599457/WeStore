b06144b874b450dfad3b9dbf2e0029147fd7dad2
binbin <729599457@qq.com>
2017-09-28 17:27:26 +0800

    新增下订单功能

--- src/main/java/com/westore/controller/OrderController.java
+++ src/main/java/com/westore/controller/OrderController.java
@@ -2,11 +2,16 @@ package com.westore.controller;
 
 import com.westore.model.AjaxJSON;
 import com.westore.model.T_B_Order;
+import com.westore.model.T_B_Order_Detail;
 import com.westore.service.*;
 import com.westore.service.service.impl.CommomServiceImpl;
 import com.westore.utils.CustomUUID;
+import net.sf.json.JSONArray;
+import net.sf.json.JSONObject;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import javax.annotation.Resource;
@@ -20,6 +25,8 @@ public class OrderController {
 
     @Resource
     private OrderService orderService;
+    @Resource
+    private CommomService commomService;
 
     @RequestMapping(value = "/getOrders.do")
     @ResponseBody
@@ -51,30 +58,31 @@ public class OrderController {
 
     @RequestMapping(value = "/addOrder.do")
     @ResponseBody
-    public AjaxJSON addOrder(HttpServletRequest request){
+    public AjaxJSON addOrder(@RequestParam Map<String,Object> params, @RequestBody AjaxJSON obj){
         AjaxJSON j=new AjaxJSON();
 
         try {
 
-            String user_id = request.getParameter("user_id") == null ? "" : request.getParameter("user_id");
-            String total_money = request.getParameter("total_money") == null ? "" : request.getParameter("total_money");
-            String order_date = request.getParameter("order_date") == null ? "" : request.getParameter("order_date");
-            String order_state = request.getParameter("order_state") == null ? "" : request.getParameter("order_state");
+            String user_id= (String) params.get("user_id");
 
             if(user_id==null&&user_id.equals("")){
                 j.setMsg("user_id为空");
                 j.setSuccess(false);
                 return j;
             }
+            //获得商品详情列表
+            List t_b_order_detail_list= (List) JSONArray.fromObject(obj.getObj());
 
-            Map<String, Object> paraMap = new HashMap<String, Object>();
-            paraMap.put("id", CustomUUID.getFlowIdWorkerInstance().generate());
-            paraMap.put("user_id", user_id);
-            paraMap.put("total_money",total_money);
-            paraMap.put("order_date",order_date);
-            paraMap.put("order_state",order_state);
 
-            orderService.addOrder(paraMap);
+            T_B_Order t_b_order=new T_B_Order();
+            t_b_order.setId(CustomUUID.getFlowIdWorkerInstance().generate());
+            t_b_order.setOrder_date(new Date());
+            t_b_order.setOrder_state(0);
+
+            commomService.add(t_b_order);
+            j.setObj(t_b_order);
+            j.setMsg(" 订单生成成功");
+
 
         }catch (Exception e){
             j.setMsg(e.getMessage());
---
+++ src/main/java/com/westore/model/T_B_Order_Detail.java
@@ -0,0 +1,44 @@
+package com.westore.model;
+
+public class T_B_Order_Detail {
+
+    private String id;
+    private String order_id;
+    private String goods_id;
+    private int goods_num;
+
+    public T_B_Order_Detail() {
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getOrder_id() {
+        return order_id;
+    }
+
+    public void setOrder_id(String order_id) {
+        this.order_id = order_id;
+    }
+
+    public String getGoods_id() {
+        return goods_id;
+    }
+
+    public void setGoods_id(String goods_id) {
+        this.goods_id = goods_id;
+    }
+
+    public int getGoods_num() {
+        return goods_num;
+    }
+
+    public void setGoods_num(int goods_num) {
+        this.goods_num = goods_num;
+    }
+}
