7ff0a04e6ce8b357f5df36d26132b484efc2a61c
binbin <729599457@qq.com>
2017-10-20 15:32:47 +0800

    HashMap Key-values测试

--- src/main/java/com/westore/utils/Test.java
+++ src/main/java/com/westore/utils/Test.java
@@ -5,15 +5,12 @@ import com.westore.model.T_B_Goods_Type;
 import com.westore.service.CommomService;
 
 import javax.annotation.Resource;
-import java.util.ArrayList;
-import java.util.Iterator;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 public class Test {
 
     public static void main(String[] args) {
-
+/*
         List<String> stringList = new ArrayList<String>();
         stringList.add("141");
         stringList.add("141");
@@ -24,7 +21,12 @@ public class Test {
         Iterator<String> iterator=stringList.iterator();
         while (iterator.hasNext()){
             System.out.println(iterator.next());
-        }
+        }*/
+        Map<String, Object> paraMap = new HashMap<String, Object>();
+        //HashMap的Key可以为空值
+        paraMap.put(null,"null");//null
+        paraMap.put(null,"null2");//null2
+        System.out.println(paraMap.get(null));
 
     }
 
