09ff696c391ac4a9c6cb30357786b86b8d78c0ba
binbin <729599457@qq.com>
2017-10-20 15:38:17 +0800

    Iterator 测试remove()方法

--- src/main/java/com/westore/utils/Test.java
+++ src/main/java/com/westore/utils/Test.java
@@ -10,23 +10,25 @@ import java.util.*;
 public class Test {
 
     public static void main(String[] args) {
-/*
         List<String> stringList = new ArrayList<String>();
         stringList.add("141");
-        stringList.add("141");
+        stringList.add("11");
         stringList.add("141");
         stringList.add("141");
         stringList.add("141");
 
         Iterator<String> iterator=stringList.iterator();
         while (iterator.hasNext()){
-            System.out.println(iterator.next());
-        }*/
-        Map<String, Object> paraMap = new HashMap<String, Object>();
+            if(iterator.next().equals("11")){
+                iterator.remove();
+            }
+        }
+        System.out.println(stringList);
+/*        Map<String, Object> paraMap = new HashMap<String, Object>();
         //HashMap的Key可以为空值
-        paraMap.put(null,"null");//null
+        paraMap.put(null,null);//null
         paraMap.put(null,"null2");//null2
-        System.out.println(paraMap.get(null));
+        System.out.println(paraMap.get(null));*/
 
     }
 
