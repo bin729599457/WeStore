8012aaa3e1aac3edf0841352f20d931461f9e3f3
binbin <729599457@qq.com>
2017-10-27 09:04:05 +0800

    map Entry的基本用法，entry.getKey(),entry.getValue()

--- src/main/java/com/westore/utils/Test.java
+++ src/main/java/com/westore/utils/Test.java
@@ -10,10 +10,17 @@ import java.util.*;
 public class Test {
 
     public static void main(String[] args) {
-        Map map = new HashMap();
-        map.put(1l, "abc");
-        System.out.println(map.get(1));
-        System.out.println(map.get(1l));
+        Map<String, Integer> map = new HashMap<String, Integer>();
+        map.put(null, 0);
+        map.put("java", 1);
+        map.put("c++", 2);
+        map.put("python", 3);
+        map.put("php", 4);
+        map.put("nodejs", 5);
+        for(Map.Entry<String, Integer> entry : map.entrySet()) {
+            System.out.println(entry.getKey() + ": " + entry.getValue());
+        }
+        System.out.println("php".hashCode() == "c++".hashCode());
 
     }
 
