3b306ff392571d036beeaa221508271df4cb72b2
binbin <729599457@qq.com>
2017-11-03 09:58:02 +0800

    选择排序

--- src/main/java/com/westore/utils/Test.java
+++ src/main/java/com/westore/utils/Test.java
@@ -13,29 +13,39 @@ public class Test {
     private String testStr="hello";
 
     public static void main(String[] args) {
-        /*
-        Map<String, Integer> map = new HashMap<String, Integer>();
-        map.put(null, 0);
-        map.put("java", 1);
-        map.put("c++", 2);
-        map.put("python", 3);
-        map.put("php", 4);
-        map.put("nodejs", 5);
-        for(Map.Entry<String, Integer> entry : map.entrySet()) {
-            System.out.println(entry.getKey() + ": " + entry.getValue());
+
+        int[] ints={5,3,8,6,2,5,12,63};
+        selectSort(ints);
+
+        for(int i:ints){
+            System.out.println(i);
         }
-        System.out.println("php".hashCode() == "c++".hashCode());*/
-
-        ArrayList arrayList=new ArrayList();
-        arrayList.add("A");
-        arrayList.add("B");
-        arrayList.add("a");
-
-        Collections.reverse(arrayList);
-        System.out.println(arrayList);// 输出结果为[B, A]
-
 
     }
 
+    public static void selectSort(int[] arr) {
+        if(arr == null || arr.length == 0)
+            return ;
+        int minIndex = 0;
+        for(int i=0; i<arr.length-1; i++) { //只需要比较n-1次
+            minIndex = i;
+            for(int j=i+1; j<arr.length; j++) { //从i+1开始比较，因为minIndex默认为i了，i就没必要比了。
+                if(arr[j] <arr[minIndex]) {
+                    minIndex = j;
+                }
+            }
+
+            if(minIndex != i) { //如果minIndex不为i，说明找到了更小的值，交换之。
+                swap(arr, i, minIndex);
+            }
+        }
+
+    }
+
+    public static void swap(int[] arr, int i, int j) {
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+    }
 
 }
