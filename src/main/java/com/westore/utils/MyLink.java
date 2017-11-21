86234a8b4f4d245dcd3b0adf18e51c6265c66c84
binbin <729599457@qq.com>
2017-11-21 23:27:08 +0800

    //方法：获取单链表的长度

--- src/main/java/com/westore/utils/MyLink.java
+++ src/main/java/com/westore/utils/MyLink.java
@@ -132,4 +132,20 @@ public class MyLink {
             current = current.next;
             }
          }
+
+    //方法：获取单链表的长度
+    public int getLength(Node head) {
+        if (head == null) {
+            return 0;
+        }
+
+        int length = 0;
+        Node current = head;
+        while (current != null) {
+            length++;
+            current = current.next;
+        }
+
+        return length;
+    }
 }
