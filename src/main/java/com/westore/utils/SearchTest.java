ccfce64b7334eb8b417d26fab267cec4ca72801b
binbin <729599457@qq.com>
2017-12-04 23:36:44 +0800

    二分查找（二）

--- src/main/java/com/westore/utils/SearchTest.java
+++ src/main/java/com/westore/utils/SearchTest.java
@@ -27,4 +27,26 @@ public class SearchTest {
         }
         return -1;
     }
+    /**
+     * 二分查找递归实现。
+     * @param srcArray  有序数组
+     * @param start 数组低地址下标
+     * @param end   数组高地址下标
+     * @param key  查找元素
+     * @return 查找元素不存在返回-1
+     */
+    public static int binSearch(int srcArray[], int start, int end, int key) {
+        int mid = (end - start) / 2 + start;
+        if (srcArray[mid] == key) {
+            return mid;
+        }
+        if (start >= end) {
+            return -1;
+        } else if (key > srcArray[mid]) {
+            return binSearch(srcArray, mid + 1, end, key);
+        } else if (key < srcArray[mid]) {
+            return binSearch(srcArray, start, mid - 1, key);
+        }
+        return -1;
+    }
 }
