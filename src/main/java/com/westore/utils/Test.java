9a114758928e95260a7061355a27b13afbb3ec45
binbin <729599457@qq.com>
2017-12-05 23:10:40 +0800

    测试接口和抽象类的区别（一）

--- src/main/java/com/westore/utils/Test.java
+++ src/main/java/com/westore/utils/Test.java
@@ -7,34 +7,38 @@ import com.westore.service.CommomService;
 import javax.annotation.Resource;
 import java.util.*;
 
-public class Test {
 
-    private int testNum=0;
-    private String testStr="hello";
-
-    public static void main(String[] args) {
-/*
-        Map map=new TreeMap();
-        map.put(1, "a");
-        map.put(6, "b");
-        map.put(2, "c");
-        map.put(5, "d");
-        map.put(6,"f");
-
-
-        Set entries = map.entrySet( );
-        if(entries != null) {
-            Iterator iterator = entries.iterator();
-            while (iterator.hasNext()) {
-                Map.Entry entry = (Map.Entry) iterator.next();
-                Object key = entry.getKey();
-                Object value = entry.getValue();
-//                System.out.println(key.toString()+""+value.toString());
-            }
-        }*/
-
+    // 抽象类Animal，包含了一个抽象方法cry
+    abstract class Animal
+    {
+        public abstract void cry();
     }
 
+    // 子类Dog继承的抽象类Animal，必须实现其抽象方法cry
+    class Dog extends Animal
+    {
+        public void cry()
+        {
+            System.out.println("Dog cry");
+        }
+    }
+    // 同样，子类Cat继承的抽象类Animal，必须实现其抽象方法cry
+    class Cat extends Animal
+    {
+        public void cry()
+        {
+            System.out.println("Cat cry");
+        }
+    }
 
+    public class Test
+    {
+        public static void main(String[] args) {
+            Animal a1 = new Dog();  // 抽象类引用指向子类实例
+            Animal a2 = new Cat();
 
-}
+            a1.cry();
+            a2.cry();
+        }
+    }
+
