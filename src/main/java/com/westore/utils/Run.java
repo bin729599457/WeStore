f1d4ebcd2b46628348cf0b183a952c2b4ed7ae43
binbin <729599457@qq.com>
2017-12-11 19:55:13 +0800

    测试线程是否中断.

---
+++ src/main/java/com/westore/utils/Run.java
@@ -0,0 +1,17 @@
+package com.westore.utils;
+
+public class Run {
+    public static void main(String args[]){
+        Thread thread = new MyThread();
+        thread.start();
+        try {
+            Thread.sleep(2000);
+            thread.interrupt();
+
+            System.out.println("stop 1??" + thread.interrupted());
+            System.out.println("stop 2??" + thread.interrupted());
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+}
