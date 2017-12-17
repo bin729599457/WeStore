52d5011ded8ef855610a8808741d665e9d8cf8ac
binbin <729599457@qq.com>
2017-12-17 23:39:55 +0800

    单链表反转练习

---
+++ src/main/java/com/westore/model/Node.java
@@ -0,0 +1,11 @@
+package com.westore.model;
+
+public class Node{
+    int value;
+    Node nextNode;
+    public Node(int value, Node nextNode) {
+        super();
+        this.value = value;
+        this.nextNode = nextNode;
+    }
+}
