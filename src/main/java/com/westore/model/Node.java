dc6d42fa76a366ea8db9496ac9a2c168180c3ccb
binbin <729599457@qq.com>
2017-12-17 23:40:13 +0800

    ..

--- src/main/java/com/westore/model/Node.java
+++ src/main/java/com/westore/model/Node.java
@@ -8,4 +8,25 @@ public class Node{
         this.value = value;
         this.nextNode = nextNode;
     }
+
+    /**
+     * 初始化单链表
+     * @param num 数量
+     * @return
+     */
+    public static Node init(int num) {
+        Node node = new Node(0, null);
+        Node cur = null;
+        Node temp = null;
+        for(int i = 1 ; i < num;i++){
+            temp = new Node(i, null);
+            if (i == 1) {
+                node.nextNode = temp;
+            }else{
+                cur.nextNode = temp;
+            }
+            cur = temp;
+        }
+        return node;
+    }
 }
