be3baca98faffa29f614f0e5d4b34ab896018ae1
binbin <729599457@qq.com>
2017-12-26 23:21:40 +0800

    test subString()

--- src/main/java/com/westore/utils/Test.java
+++ src/main/java/com/westore/utils/Test.java
@@ -4,10 +4,23 @@ public class Test {
 
     public static void main(String[] args) {
 
-        String testStr="测试test";
-        testStr+="(1)";
-        testStr.substring(0,2);
-        System.out.println(testStr.substring(0,3));
-
+        String str="this is my original string";
+        String toDelete=" original";
+        if(str.startsWith(toDelete))
+            str=str.substring(toDelete.length());
+        else
+            if(str.endsWith(toDelete))
+                str=str.substring(0, str.length()-toDelete.length());
+            else {
+                int index=str.indexOf(toDelete);
+                if(index!=-1) {
+                    String str1=str.substring(0, index);
+                    String str2=str.substring(index+toDelete.length());
+                    str=str1+str2;
+                    }
+                else
+                    System.out.println("string /"+""+toDelete+"/"+" not found");
+            }
+        System.out.println(str);
     }
 }
