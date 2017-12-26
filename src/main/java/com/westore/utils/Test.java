ca4d540ac02ebcfb2bc1388a717c5bcb68a791b2
binbin <729599457@qq.com>
2017-12-26 23:23:00 +0800

    ;...

--- src/main/java/com/westore/utils/Test.java
+++ src/main/java/com/westore/utils/Test.java
@@ -3,7 +3,10 @@ package com.westore.utils;
 public class Test {
 
     public static void main(String[] args) {
-
+/**
+ *      str＝str.substring(int beginIndex);截取掉str从首字母起长度为beginIndex的字符串，将剩余字符串赋值给str；
+        str＝str.substring(int beginIndex，int endIndex);截取str中从beginIndex开始至endIndex结束时的字符串，并将其赋值给str;
+ */
         String str="this is my original string";
         String toDelete=" original";
         if(str.startsWith(toDelete))
