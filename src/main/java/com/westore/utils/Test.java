544d3201892c68bf692fde0cd703d0b1f73bef56
binbin <729599457@qq.com>
2017-12-26 23:17:45 +0800

    substring练习

---
+++ src/main/java/com/westore/utils/Test.java
@@ -0,0 +1,12 @@
+package com.westore.utils;
+
+public class Test {
+
+    public static void main(String[] args) {
+
+        String testStr="测试test";
+        testStr+="(1)";
+        testStr.substring(0,2);
+        System.out.println(testStr.substring(0,1));
+    }
+}
