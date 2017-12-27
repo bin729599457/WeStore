d80caa0cfb39d26cbd6273ac98633486cba8717e
binbin <729599457@qq.com>
2017-12-27 23:40:14 +0800

    ...

--- src/main/java/com/westore/utils/Test.java
+++ src/main/java/com/westore/utils/Test.java
@@ -7,7 +7,7 @@ public class Test {
  *      str＝str.substring(int beginIndex);截取掉str从首字母起长度为beginIndex的字符串，将剩余字符串赋值给str；
         str＝str.substring(int beginIndex，int endIndex);截取str中从beginIndex开始至endIndex结束时的字符串，并将其赋值给str;
  */
-        String str="this is my original string";
+/*        String str="this is my original string";
         String toDelete=" original";
         if(str.startsWith(toDelete))
             str=str.substring(toDelete.length());
@@ -24,7 +24,7 @@ public class Test {
                 else
                     System.out.println("string /"+""+toDelete+"/"+" not found");
             }
-        System.out.println(str);
+        System.out.println(str);*/
 
         /**
          * 联系substring
